6abf14a61c9eeff5e32472e7cdc77a04d235f979
brnydr <brianwilliamyoder@gmail.com>
2023-07-02 11:54:38 -0700

    add address book constructor and method

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
+function AddressBook() {
+    this.contacts = {};
+}
 
-function Contact(firstName, LastName, phoneNumber) {
+AddressBook.prototype.addContact = function(contact) {
+    this.contacts[contact.firstName] = contact;
+}
+
+function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
@@ -9,7 +16,9 @@ Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
 }   
 
-
+// handleFormSubmission will be called when the form is submitted, and will call
+// the other functions to create a new contact, add it to the list, and display it
+// using UI functions.
 function handleFormSubmission () {
 
 }
