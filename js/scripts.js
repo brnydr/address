0916f7497b64ac1147b7282ce8f3d1f5d70c8ba3
brnydr <brianwilliamyoder@gmail.com>
2023-07-02 12:08:42 -0700

    add addContact, assignId findContact deleteContact

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 function AddressBook() {
     this.contacts = {};
+    this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
-    this.contacts[contact.firstName] = contact;
+    this.currentId = this.assignId();
+    this.contacts[this.currentId] = contact;
 }
+
+AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+    if (this.contacts[id] !== undefined) {
+      return this.contacts[id];
+    }
+    return false;
+  };
+
+  AddressBook.prototype.deleteContact = function(id) {
+    if (this.contacts[id] === undefined) {
+      return false;
+    }
+    delete this.contacts[id];
+    return true;
+  };
 
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
